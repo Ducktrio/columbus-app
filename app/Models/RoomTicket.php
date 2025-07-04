21ac539da29d0497f10448e99bf474a3e4706ddd
Lawrance Cancerlon <lawrance.cancerlon@binus.ac.id>
2025-07-05 04:13:20 +0700

    Added model specific methods

--- app/Models/RoomTicket.php
+++ app/Models/RoomTicket.php
@@ -55,4 +55,34 @@ class RoomTicket extends Model
     {
         return $this->belongsTo(User::class, 'customer_id');
     }
+    
+    public function check_in()
+    {
+        $this->check_in_date = now();
+        $this->save();
+
+        if ($this->room) {
+            $this->room->status = 1;
+            $this->room->save();
+        }
+    }
+
+    public function check_out()
+    {
+        $this->check_out_date = now();
+        $this->status = 1;
+        $this->save();
+
+        if ($this->room) {
+            $this->room->status = 2;
+            $this->room->save();
+        }
+
+        ServiceTicket::create([
+            'customer_id' => $this->customer_id,
+            'room_id' => $this->room_id,
+            'service_id' => Service::where('name', 'Cleaning')->first()->id,
+            'details' => 'Room checkout cleaning service',
+        ]);
+    }
 }
